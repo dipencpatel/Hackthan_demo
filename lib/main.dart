84c1f3b50c13b2abb5fe473cfa13fb49a73de11b
Miral <miral.shah@volansystech.com>
2020-06-27 15:02:17 +0530

    firebase_database added
    - added firebase_database
    - remvoed firebase cloud storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 ///added this to check all dependancies are downloaded or not
 
 void main() {
